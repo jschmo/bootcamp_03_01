56cff6ce22c6acc9f1d59a35204c43e45be65708
Alison Veras <alisson_veras@hotmail.com>
2020-07-23 00:21:57 -0300

    02_01-Criando Rotas

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Dashboard () {}
+// const Dashboard: React.FunctionComponent = () => {};
+const Dashboard: React.FC = () => {
+  return <h1>Dashboard</h1>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// function Dashboard () {}
+// const Dashboard: React.FunctionComponent = () => {};
+const Repository: React.FC = () => {
+  return <h1>Repository</h1>;
+};
+
+export default Repository;
